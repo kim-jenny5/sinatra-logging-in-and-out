89b1f18d88a4389298c761836bb808f868848d00
Jenny Kim <kimj1905@gmail.com>
2021-05-31 16:23:43 -0400

    commit tree 5197646cf42736a022d43f349ee0fc8d62047dc1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+class User < ActiveRecord::Base
+end
 
+# <% if Helpers.is_logged_in?(session) %>
+#     <% if Helpers.current_user(session) %>
+#       # <h1>Welcome <%= Helpers.current_user(session).username%></h1>
+#       # <h1>Your Balance <%= Helpers.current_user(session).balance%></h1>
+#       <h1>Welcome <%= @user.username%></h1>
+#       <h2>Your Balance: <%= @user.balance%></h2>
+#       <% end %>
+#   <h3><a href="/logout">Log Out</a></h3>
+#   <% end %>
 
+# <% if Helpers.is_logged_in?(session) %>
+#     <% if Helpers.current_user(session) %>
+#       <h1>Welcome <%= @user.username%></h1>
+#       <h3>Your Balance: <%= @user.balance%></h3>
+#     <% end %>
+#     <h4><a href="/logout">Log Out</a></h4>
+#   <% end %>
